d10ef5c3623214495e832db52f3482ddbfc9cd9e
liuh26 <liuh26@VUSE-F70WTH3.vuds.vanderbilt.edu>
2024-01-10 00:00:14 -0600

    add playground

--- synthesis/options/test_options.py
+++ synthesis/options/test_options.py
@@ -10,8 +10,7 @@ class TestOptions(BaseOptions):
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)  # define shared options
         parser.add_argument('-c', '--code', nargs='*', required=True, type=float, help='which code is used for controllable style generation.')
-        parser.add_argument('-i', '--image_dir', type=str, required=True, 
-            help='where the source images are stored. Assume that labels are saved in the folder name by replacing "Images" to "Labels"')
+        parser.add_argument('-i', '--image_dir', type=str, help='where the source images are stored. Assume that labels are saved in the folder name by replacing "Images" to "Labels"')
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
         parser.add_argument('--sw_batch_size', type=int, default=1, help='sub-batch size of sliding window inference.')
         parser.add_argument('--overlap', type=float, default=0.7, help='overlapping ratio of sliding window inference.')
@@ -19,5 +18,11 @@ class TestOptions(BaseOptions):
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
         parser.add_argument('--num_test', type=int, default=20, help='how many test images to run')
+
+        # playground
+        parser.add_argument('--src_image', type=str, help='the path of a single source domain image.')
+        parser.add_argument('--src_label', type=str, help='the path of the source domain label.')
+        parser.add_argument('--save_dir',  type=str, help='the directory to save the output image for the playground.')
+
         self.isTrain = False
         return parser
